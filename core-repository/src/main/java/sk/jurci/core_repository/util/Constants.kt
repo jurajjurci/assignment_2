e610021dbc1376cfeb5f9cd4f9d48ceba8944785
Juraj Jurči <juraj.jurci@gmail.com>
2024-09-17 11:14:02 +0200

    movie's image path refactored

--- core-repository/src/main/java/sk/jurci/core_repository/util/Constants.kt
+++ core-repository/src/main/java/sk/jurci/core_repository/util/Constants.kt
@@ -2,4 +2,6 @@ package sk.jurci.core_repository.util
 
 internal object Constants {
     const val MOVIE_PAGE_SIZE = 20
+
+    const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p"
 }
--- feature-movie/src/main/java/sk/jurci/feature_movie/movie_list/MovieItem.kt
+++ feature-movie/src/main/java/sk/jurci/feature_movie/movie_list/MovieItem.kt
@@ -39,10 +39,9 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import sk.jurci.core_repository.model.Movie
 import sk.jurci.core_repository.model.Movie.Companion.DEMO_MOVIE
+import sk.jurci.core_repository.model.image.PosterSize
 import sk.jurci.feature_movie.R
 import sk.jurci.feature_movie.ui.theme.Dimensions
-import sk.jurci.feature_movie.utils.Constants
-import sk.jurci.feature_movie.utils.convertPosterPathToImageUrl
 
 @Preview
 @Composable
@@ -77,7 +76,7 @@ fun MovieItem(
             }) {
             AsyncImage(
                 modifier = Modifier.fillMaxSize(),
-                model = convertPosterPathToImageUrl(movie.posterPath, Constants.PosterUrlSize.w154),
+                model = movie.posterPath.toUrl(PosterSize.W_154),
                 contentScale = ContentScale.Crop,
                 contentDescription = movie.title,
             )
